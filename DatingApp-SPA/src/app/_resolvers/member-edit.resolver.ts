2976d46c9eb74a0bf05251d0bd1ca615f8f88c7f
LAPTOP-NT2BFFVB\markd <markda69@hotmail.com>
2018-11-16 15:17:12 -0800

    Updating user

--- DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -5,18 +5,20 @@ import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
-export class MemberDetailResolver implements Resolve<User> {
+export class MemberEditResolver implements Resolve<User> {
     constructor(
         private userService: UserService,
         private router: Router,
-        private alertify: AlertifyService) {}
+        private alertify: AlertifyService,
+        private authService: AuthService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(route.params['id']).pipe(
+        return this.userService.getUser(this.authService.decodeToken.nameid).pipe(
             catchError(error => {
-                this.alertify.error('Problem returning your data');
+                this.alertify.error(error);
                 this.router.navigate(['members/']);
                 return of(null);
             })
--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -8,13 +8,20 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
-      { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+      { path: 'members', component: MemberListComponent,
+        resolve: {users: MemberListResolver}},
+      { path: 'members/:id', component: MemberDetailComponent,
+        resolve: {user: MemberDetailResolver}},
+      { path: 'member/edit', component: MemberEditComponent,
+        resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChangesGuard]},
       { path: 'messages', component: MessagesComponent},
       { path: 'lists', component: ListsComponent}
     ]},
